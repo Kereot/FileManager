4d5341e62f0f4e6bc9a784a7a7d8013ba95888c0
Kereot <eepdmitriy@gmail.com>
2022-04-22 17:55:45 +0300

    GUI basis

---
+++ client/src/main/java/ru/gb/filemanager/CommonMessages.java
@@ -0,0 +1,6 @@
+package main.java.ru.gb.filemanager;
+
+public class CommonMessages {
+    public static final String ABORT = "Operation aborted";
+    public static final String INACTIVE = "No element selected in the active window";
+}
